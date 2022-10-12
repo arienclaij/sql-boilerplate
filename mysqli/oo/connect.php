f3ab330ea27b03ce828ff9036afd00f66a4a0974
clay_ <clay_@DESKTOP-PPJ70Q4.fritz.box>
2022-10-13 01:05:59 +0200

    Eerste opzetje van Engelse comments in oo/connect.php. First draft of English comments in oo/connect.php

--- mysqli/oo/connect.php
+++ mysqli/oo/connect.php
@@ -1,59 +1,60 @@
 <?php
 /*
- * MySQLi CONNECTIE BESTAND
- * In dit bestand maken we de verbinding met een MySQL
- * server doormiddel van MySQLi functies. In deze
- * functie zit meteen het selecteren van een db, dus
- * dat doen we ook meteen.
+  * MySQLi CONNECTION FILE
+  * In this file we make the connection with a MySQL
+  * server through MySQLi functions. In this
+  * function is immediately selecting a db, so
+  * we do that right away.
  */
 
 /* Error-Handling
  * ==============
- * Als eerst maken we een functie voor het beheren van
- * errors. Hiermee kunnen we makkelijk het probleem vinden
- * als er iets mis is.
- * Zodra een script online is wil je niet dat de gebruiker
- * de foutmeldingen krijgt, vandaar dat we hier error_log
- * gebruiken als DEBUG_MODE false is.
+  * First, let's create a function to manage the
+  * errors. With this we can easily find the problem
+  * if something is wrong.
+  * Once a script is online you don't want the user
+  * get the error messages, that's why we put error_log here
+  * use if DEBUG_MODE is false.
  */
-// DEBUG_MODE, deze wordt false als het script online geplaatst is
+
+// DEBUG_MODE, this will be false if the script is placed online
 define('DEBUG_MODE', true);
 
 /*
- * We slaan alle errors op in $errors. Deze lezen we in het
- * script uit in via een foreach loop
+  * We store all errors in $errors. We read this in the
+  * script out via a foreach loop
  */
 $errors = array();
 
 if (DEBUG_MODE) { 
-    // DEBUG_MODE staat aan
-	// Zorg dat we alle errors te zien krijgen
+    // DEBUG_MODE is on
+	// Make sure we see all errors
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL | E_NOTICE);
 } else {
-	// DEBUG_MODE uit dus geen errors tonen
+	// DEBUG_MODE is off, don't show arrors
 	ini_set('display_errors', 'Off');
 	error_reporting(0);
 }
 
 /*
- * Een functie zodat we makkelijk en mooi errors kunnen tonen
+ * A function so that we can easily and beautifully show errors
  */
 function SQLerror($error, $message, $file)
 {
-	// $error is het resultaat van MySQLi::error()
-	// $message is de tekst die bij de error staat, 
-	//          deze tekst zullen we gebruiken als DEBUG_MODE uit staat
-	// $file is het resultaat van __FILE__ in het bestand van de error
+	// $error is the result of MySQLi::error()
+        // $message is the text accompanying the error,
+        // we will use this text if DEBUG_MODE is off
+        // $file is the result of __FILE__ in the file of the error
 	
-	global $errors; // Zorg dat de error variabele die we net hebben gemaakt in deze functie komt
+	global $errors; // Put the error variable we just created in this function
 
     if (DEBUG_MODE) { 
-        // DEBUG_MODE aan => sla de errors op zodat we ze later kunnen tonen
+        // DEBUG_MODE on => store the errors so we can show them later
 		$errors[] = $message.': '.$error;
     } else { 
-        // DEBUG_MODE uit => log de errors en sla alleen de $message op
-		// We slaan niet alleen de error op, maar ook het bestand en de datum
+        // DEBUG_MODE off => log the errors en store only $message
+		// We store not only the error, but also the file and the date for the convenience
 		$log = $file.' ['.date('H:i:s').'] '.$error;
 		error_log($log);
 
@@ -61,14 +62,14 @@ function SQLerror($error, $message, $file)
 	}
 }
 
-/* CONNECTIE MET MySQL SERVER
+/* Connection with MySQLi
  * ==========================
  */
 $sqlLink = new MySQLi('localhost', 'username', 'password', 'sql-boilerplate');
-// Verander de host, inlog naam, wachtwoord en sql-boilerplate (database) in de juiste gegevens
+//Change the host, username, password and sql-boilerplate (database) to the correct data.
 
 if ($sqlLink === false) {
-	// Als MySQLi false returned is er iets mis gegaan, gebruik de net gemaakte error functie
-	// Omdat het een verbinding is maken we gebruik van MySQLi::connect_error
+	// If MySQLi returned false something went wrong, use the error function you just created
+        // Because it is a connection we use MySQLi::connect_error
 	SQLerror( $sqlLink->connect_error(), 'We kunnen geen verbinding aanmaken', __FILE__ );
 }
